8b0325ca6b18893f2932464a1ae5adbfd73be7bb
Ana Judd <ajudd1@ufl.edu>
2020-08-21 14:17:37 -0500

    added forms and views to users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views import generic
+from django.urls import reverse_lazy
+from .forms import RegistrationForm
 
 # Create your views here.
+
+def register(response):
+
+    if response.method=="POST":
+        form = RegistrationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/login')
+    else:
+        form = RegistrationForm()
+
+    return render(response, "registration/register.html", {"form":form})
