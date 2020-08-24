0b75d240ca3427026ca4069e1284eb41c6ae5be9
Ana Judd <ajudd1@ufl.edu>
2020-08-23 20:55:09 -0500

    updating form validation

--- users/forms.py
+++ users/forms.py
@@ -2,12 +2,59 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
 
 class RegistrationForm(UserCreationForm):
     email=forms.EmailField()
     first_name=forms.CharField(max_length = 255)
     last_name=forms.CharField(max_length = 255)
 
+
     class Meta:
         model=User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class CustomUserForm (forms.Form):
+    username = forms.CharField(max_length = 255)
+    email = forms.EmailField()
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):
+        print("email")
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise ValidationError("Email already exists")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise ValidationError("Username already exists")
+        return username
+    
+    def clean_password2(self):
+        print("TEST")
+        password1 = self.cleaned_data.get('password1')
+        print(password1, "1")
+
+        password2 = self.cleaned_data.get('password2')
+        print(password2, "2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+
+
+        return password2
+    
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['email'],
+            self.cleaned_data['username'],
+            self.cleaned_data['password1'],
+        )
+        return user
--- users/views.py
+++ users/views.py
@@ -2,18 +2,60 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.views import generic
 from django.urls import reverse_lazy
-from .forms import RegistrationForm
+from .forms import RegistrationForm, CustomUserForm
+
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
-def register(response):
 
-    if response.method=="POST":
-        form = RegistrationForm(response.POST)
+
+
+def register(request):
+
+    if request.method=="POST":
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/login')
+            return redirect('/login')
+        else:
+            print("Not valid")
+
     else:
         form = RegistrationForm()
 
-    return render(response, "registration/register.html", {"form":form})
+
+    return render(request, "registration/register.html", {"form":form})
+
+'''
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST.get('username')
+        email = request.POST['email']
+        baby_name = request.POST['baby_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            print("First Check")
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken')
+                return(redirect('register'))
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return(redirect('/'))
+            else:
+                user = User.objects.create_user(username=request.POST.get('username'), password=password1, email=email, first_name=first_name, last_name=last_name, baby_name=baby_name)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, "Password doesn't match")
+            return(redirect('/'))
+
+
+    else:
+        return render(request, 'registration/register.html')
+'''
